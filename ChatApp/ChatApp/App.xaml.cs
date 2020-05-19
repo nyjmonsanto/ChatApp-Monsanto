31e51206b30d03f4fbc9bc0ccd9697a1e54b9887
nyjmonsanto115 <60591167+nyjmonsanto115@users.noreply.github.com>
2020-05-20 00:24:53 +0800

    Add files via upload

---
+++ ChatApp/ChatApp/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var isLoggedIn = Properties.ContainsKey("isLoggedIn") ? (bool)Properties["isLoggedIn"] : false;
+
+            if (isLoggedIn)
+            {
+                MainPage = new Pages.MainPage();
+            }
+            else
+            {
+                MainPage = new NavigationPage(new Pages.LoginPage());
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
