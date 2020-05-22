0a39564d149df2918890cdfa22c4eedc72721425
Nigel Monsanto <nigel.monsanto@gmail.com>
2020-05-22 17:21:54 +0800

    Edited some text

--- ChatApp/ChatApp/Helpers/ScaleHelper/ScaleCS.cs
+++ ChatApp/ChatApp/Helpers/ScaleHelper/ScaleCS.cs
@@ -5,12 +5,6 @@ namespace ChatApp.Helpers.ScaleHelper
 {
     public static class ScaleCS
     {
-        /// <summary>
-        /// Scales the height.
-        /// </summary>
-        /// <returns>The height.</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
         public static float ScaleHeight(this int number, int? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
@@ -19,12 +13,6 @@ namespace ChatApp.Helpers.ScaleHelper
             return (float)(number * (App.screenHeight / 568.0));
         }
 
-        /// <summary>
-        /// Scales the height.
-        /// </summary>
-        /// <returns>The height.</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
         public static float ScaleHeight(this double number, double? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
@@ -33,12 +21,6 @@ namespace ChatApp.Helpers.ScaleHelper
             return (float)(number * (App.screenHeight / 568.0));
         }
 
-        /// <summary>
-        /// Scales the height.
-        /// </summary>
-        /// <returns>The height.</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
         public static float ScaleHeight(this float number, float? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
@@ -47,12 +29,7 @@ namespace ChatApp.Helpers.ScaleHelper
             return (float)(number * (App.screenHeight / 568.0));
         }
 
-        /// <summary>
-        /// Scales the width.
-        /// </summary>
-        /// <returns>The width.</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
+
         public static float ScaleWidth(this int number, int? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
@@ -61,12 +38,6 @@ namespace ChatApp.Helpers.ScaleHelper
             return (float)(number * (App.screenWidth / 320.0));
         }
 
-        /// <summary>
-        /// Scales the width.
-        /// </summary>
-        /// <returns>The width.</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
         public static float ScaleWidth(this double number, double? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
@@ -75,12 +46,6 @@ namespace ChatApp.Helpers.ScaleHelper
             return (float)(number * (App.screenWidth / 320.0));
         }
 
-        /// <summary>
-        /// Scales the width.
-        /// </summary>
-        /// <returns>The width.</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
         public static float ScaleWidth(this float number, float? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
@@ -89,12 +54,7 @@ namespace ChatApp.Helpers.ScaleHelper
             return (float)(number * (App.screenWidth / 320.0));
         }
 
-        /// <summary>
-        /// Scales the font.
-        /// </summary>
-        /// <returns>The font.</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
+
         public static double ScaleFont(this int number, int? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
@@ -103,12 +63,6 @@ namespace ChatApp.Helpers.ScaleHelper
             return (number * (App.screenHeight / 568.0) - (Device.RuntimePlatform == Device.iOS ? 0.5 : 0));
         }
 
-        /// <summary>
-        /// Scales the font.
-        /// </summary>
-        /// <returns>Formatted chips</returns>
-        /// <param name="number">Number.</param>
-        /// <param name="iOS">Value for iOS.</param>
         public static double ScaleFont(this double number, double? iOS = null)
         {
             if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
